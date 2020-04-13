a80eb142ccfa9724df3b854ca730236d3b9e25bf
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-13 21:51:09 +0300

    Add design of UICollectionView cells

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Dmitry Reshetnik on 13.04.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
