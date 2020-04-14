5385260ceb3e9772bc29ffa49493dadd462ed3c9
reshetnikd <dmitry.reshetnik@icloud.com>
2020-04-14 21:12:11 +0300

    Add Person class

---
+++ NamesToFaces/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  NamesToFaces
+//
+//  Created by Dmitry Reshetnik on 14.04.2020.
+//  Copyright © 2020 Dmitry Reshetnik. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    var people = [Person]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +53,10 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         if let jpegData = image.jpegData(compressionQuality: 0.8) {
             try? jpegData.write(to: imagePath)
         }
+        
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
         
         dismiss(animated: true)
     }
